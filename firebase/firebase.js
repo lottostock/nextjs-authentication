ba957a68069aa01116b956aad9ad600472991087
lottostock <85630215+lottostock@users.noreply.github.com>
2021-06-29 19:06:22 +0700

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,13 +1,16 @@
 import firebase from 'firebase';
 import 'firebase/auth';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "enter-api-key",
-    authDomain: "enter-auth-domain",
-    projectId: "enter-project-id",
-    storageBucket: "enter-storage-bucket",
-    messagingSenderId: "enter-messenger-id",
-    appId: "enter-app-id"
+  apiKey: "AIzaSyCSyjLFdmjeHadEWZp_odOXVeWSEYX--wc",
+  authDomain: "pnck-dev-app.firebaseapp.com",
+  databaseURL: "https://pnck-dev-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pnck-dev-app",
+  storageBucket: "pnck-dev-app.appspot.com",
+  messagingSenderId: "66416636425",
+  appId: "1:66416636425:web:227d018ef03ec5eabe3ed8",
+  measurementId: "G-L959PNZS72"
 };
 
 // Initialize Firebase
